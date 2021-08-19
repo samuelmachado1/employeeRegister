860f6ee3bfb3e712cde7c803edbaea349fe84d00
Samuel Machado <samuelmachado1@gmail.com>
2021-08-19 01:21:04 -0300

    ft(crud): Initializing CRUD made with AdonisJS. Migrations and Models already created and with the first adjustments ready.

---
+++ tls/database/migrations/1629342815531_employees.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Employees extends BaseSchema {
+  protected tableName = 'employees'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('cpf').unique().notNullable()
+      table.string('name')
+      table.string('last_name')
+      table.string('role')
+      table.string('birthday')
+      table.string('salary')
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.datetime('created_at', { useTz: true })
+      table.datetime('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
