c98a51fe30dac64579771bcd0acf8b05fa494895
Samuel Machado <samuelmachado1@gmail.com>
2021-08-19 01:27:37 -0300

    ft(crud): Adjusting office model

--- tls/app/Models/Office.ts
+++ tls/app/Models/Office.ts
@@ -5,6 +5,9 @@ export default class Office extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  public description: string
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
