b2f845c449f782d55ef506488672d9678dcc8cc0
Samuel Machado <samuelmachado1@gmail.com>
2021-08-19 14:45:15 -0300

    ft(crud): Adjusting index method to OfficesController

--- tls/app/Controllers/Http/OfficesController.ts
+++ tls/app/Controllers/Http/OfficesController.ts
@@ -1,4 +1,11 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Office from 'App/Models/Office'
+
 
 export default class OfficesController {
+    public async index({response}: HttpContextContract){
+        const offices =  await Office.query()
+        return response.status(200).json({id: offices})
+    }
 }
