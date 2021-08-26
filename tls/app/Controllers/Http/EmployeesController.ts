a0138762bb74ddaa1aa0c15e766a028350e635df
Samuel Machado <samuelmachado1@gmail.com>
2021-08-26 19:23:33 -0300

    feat(crud) adjusting migrations, model and controllers

--- tls/app/Controllers/Http/EmployeesController.ts
+++ tls/app/Controllers/Http/EmployeesController.ts
@@ -1,20 +1,22 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-import Database from '@ioc:Adonis/Lucid/Database'
 import Employee from 'App/Models/Employee'
 export default class EmployeesController {
-
-    public async index({response}: HttpContextContract){
-        const employees =  await Employee.query()
-        return response.status(200).json({employees: employees})
-    }
-
-    public async store  ({ response, request }: HttpContextContract){
-        
-        const  description = request.only(["description"])        
-        const office = await Employee.create(description)
-
-        return response.status(200).json({'resposta': 'sucess', office})
-    }
-
+  public async index({ response }: HttpContextContract) {
+    const employees = await Employee.query()
+    return response.status(200).json({ employees: employees })
+  }
+  public async store({ response, request }: HttpContextContract) {
+    const bodyRequest = request.only([
+      'office_id',
+      'cpf',
+      'name',
+      'last_name',
+      'role',
+      'birthday',
+      'salary',
+    ])
+    const employee = await Employee.create({ ...bodyRequest })
+    return response.status(200).json({ resposta: 'sucess', employee })
+  }
 }
--- tls/app/Controllers/Http/OfficesController.ts
+++ tls/app/Controllers/Http/OfficesController.ts
@@ -1,20 +1,15 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Database from '@ioc:Adonis/Lucid/Database'
 import Office from 'App/Models/Office'
-
-
 export default class OfficesController {
-    public async index({response}: HttpContextContract){
-        const offices =  await Office.query()
-        return response.status(200).json({id: offices})
-    }
+  public async index({ response }: HttpContextContract) {
+    const offices = await Office.query()
+    return response.status(200).json({ id: offices })
+  }
 
-    public async store  ({ response, request }: HttpContextContract){
-        
-        const  description = request.only(["description"])        
-        const office = await Office.create(description)
-
-        return response.status(200).json({'resposta': 'sucess', office})
-    }
+  public async store({ response, request }: HttpContextContract) {
+    const description = request.only(['description'])
+    const office = await Office.create(description)
+    return response.status(200).json({ resposta: 'sucess', office })
+}
 
 }
