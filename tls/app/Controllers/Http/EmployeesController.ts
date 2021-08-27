7b8fc00a7356848999dceb0ecbd394348cb2f704
Samuel Machado <samuelmachado1@gmail.com>
2021-08-27 00:24:54 -0300

    feat(crud) adjusting store, update, index and  delete  methods in OfficesController

--- tls/app/Controllers/Http/EmployeesController.ts
+++ tls/app/Controllers/Http/EmployeesController.ts
@@ -6,6 +6,7 @@ export default class EmployeesController {
     const employees = await Employee.query()
     return response.status(200).json({ employees: employees })
   }
+
   public async store({ response, request }: HttpContextContract) {
     const bodyRequest = request.only([
       'office_id',
@@ -23,5 +24,22 @@ export default class EmployeesController {
       last_name: employee.last_name,
       role: employee.role,
     })
+  }
+
+  public async update({ response, request, params }: HttpContextContract) {
+    const employeeData = request.only(['cpf', 'name', 'last_name', 'role', 'birthday', 'salary'])
+    await Employee.query()
+      .where('cpf', params.id)
+      .update({ ...employeeData })
+
+    const newEmployee = await Employee.query().where('cpf', params.id)
+
+    return response.status(200).json({ resposta: 'sucess', newEmployee: newEmployee })
+  }
+
+  public async destroy({ response, params }: HttpContextContract) {
+    await Employee.query().where('cpf', params.id).delete()
+
+    return response.status(200).json({ code: 200, status: 'success' })
   }
 }
